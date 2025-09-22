041b95459a018cad943f5d8dc9ac4e4c2552685f
EmmyRecent <chukwuemekaeziagulu@gmail.com>
2025-09-22 14:43:19 +0100

    initial

---
+++ components/nav.tsx
@@ -0,0 +1,19 @@
+const nav = () => {
+  return (
+    <header>
+      <div className="wrapper flex justify-between items-center">
+        <div>
+          <h3>Luxora</h3>
+        </div>
+
+        <nav>
+          <ul>
+            <li>Login</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default nav;
